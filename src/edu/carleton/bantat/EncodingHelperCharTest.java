e4212d5642a1f00062e9ac0fb25c980e33e9c5f3
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-04-05 19:41:47 -0500

    Added test file to source directory

---
+++ src/edu/carleton/bantat/EncodingHelperCharTest.java
@@ -0,0 +1,41 @@
+package edu.carleton.bantat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by torebanta on 4/4/15.
+ */
+public class EncodingHelperCharTest {
+
+    @Test
+    public void testGetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8Bytes() throws Exception {
+
+    }
+
+    @Test
+    public void testToCodePointString() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8String() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCharacterName() throws Exception {
+
+    }
+}

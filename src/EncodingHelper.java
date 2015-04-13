88ef1103beeafd0cadc6559eaa755e9858f8f913
Derek Shang <dereks@eduroam-171-118.dyn.carleton.edu>
2015-04-13 13:43:58 -0500

    Added EncodingHelper.java

---
+++ src/EncodingHelper.java
@@ -0,0 +1,12 @@
+/**
+ * @author Tore Banta
+ * @author Derek Shang
+ */
+/**
+ * This class refers to the main() method in Main.java in a deeper directory.
+ */
+public class EncodingHelper {
+    public static void main(String[] args) {
+        edu.carleton.bantat.Main.main(args);
+    }
+}
